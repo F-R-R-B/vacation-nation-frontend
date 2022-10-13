be15888e5ae5e890b33e35640664ec14a130d329
Mathieu Roberts <mathieulroberts@gmail.com>
2022-10-13 12:48:12 -0700

    foot

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ class Footer extends Component {
   render () {
     return (
       <div >
-        <div className="h-40 w-screen bg-cyan-800 flex flex-col items-center justify-center">
-          <div className="font-bold text-5xl"> FOOTER </div>
+        <div className="w-screen bg-cyan-800 flex flex-col items-center justify-center">
+          <div className="text-2xl text-cyan-100"> &copy;F.R.R.B </div>
         </div>
       </div>
     );
